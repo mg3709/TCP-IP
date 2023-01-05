5156fc567d824d40cee7826034c830480b1f11cc
kmg0205 <mg3709@naver.com>
2023-01-05 20:33:53 +0900

    yes

---
+++ JavaProject/src/회원정보/Main.java
@@ -0,0 +1,15 @@
+package 회원정보;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Member member = new Member();
+        member.setName();
+        member.setAge();
+        member.setGender();
+        member.setJobs();
+        member.getInfo();
+
+    }
+}
